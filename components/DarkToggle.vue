642f9c0cccc77a45780628aca4f33d1e455dde8f
LoTwT <709937065@qq.com>
2022-04-21 13:21:03 +0800

    chore: init

---
+++ components/DarkToggle.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const color = useColorMode()
+function toggleDark() {
+  color.preference = color.preference === "dark" ? "light" : "dark"
+}
+</script>
+
+<template>
+  <button class="!outline-none" @click="toggleDark()">
+    <div class="dark:i-carbon-moon i-carbon-sun" />
+  </button>
+</template>
